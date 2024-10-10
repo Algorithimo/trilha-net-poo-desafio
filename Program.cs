da40d558eff47aba10c94f340bb44a9b4350655c
Alexandre Filho <teste@teste.com>
2024-10-10 19:11:03 -0300

    feito

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia= new Nokia("12345678", "nokia Barato", "121131", 16);
+Iphone iphone= new Iphone("87654321", "iphone Barato", "121131", 32);
+
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smarthphone iPhone:");
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
